2270ea97009afa60a17b815132c581bf68af5ba5
JackWrightMac <32547204+JackWrightMac@users.noreply.github.com>
2019-04-04 13:00:26 -0400

    TempConverter Finished

---
+++ TempConverterJackW/TempConverterJackW/TempConverterForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace TempConverterJackW
+{
+    partial class frmTempConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStatement = new System.Windows.Forms.Label();
+            this.txtCelsius = new System.Windows.Forms.TextBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblStatement
+            // 
+            this.lblStatement.AutoSize = true;
+            this.lblStatement.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblStatement.Location = new System.Drawing.Point(12, 54);
+            this.lblStatement.Name = "lblStatement";
+            this.lblStatement.Size = new System.Drawing.Size(470, 20);
+            this.lblStatement.TabIndex = 0;
+            this.lblStatement.Text = "Enter a temperature in Celsius that you would like to be converted";
+            // 
+            // txtCelsius
+            // 
+            this.txtCelsius.Location = new System.Drawing.Point(488, 54);
+            this.txtCelsius.Name = "txtCelsius";
+            this.txtCelsius.Size = new System.Drawing.Size(100, 20);
+            this.txtCelsius.TabIndex = 1;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblAnswer.Location = new System.Drawing.Point(251, 260);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(251, 20);
+            this.lblAnswer.TabIndex = 2;
+            this.lblAnswer.Text = "The temperature in fahrenheit is: ?";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(273, 167);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // frmTempConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 423);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.txtCelsius);
+            this.Controls.Add(this.lblStatement);
+            this.Name = "frmTempConverter";
+            this.Text = "Temp Converter by Jack W";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatement;
+        private System.Windows.Forms.TextBox txtCelsius;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ TempConverterJackW/TempConverterJackW/TempConverterForm.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Created by: Jack Wright
+ * Created on: 04-04-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #27 - Temperature Converter
+ * This program calculates the temperature in Fahrenheit given a temp in Celsius
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TempConverterJackW
+{
+    public partial class frmTempConverter : Form
+    {
+        public frmTempConverter()
+        {
+            InitializeComponent();
+        }
+
+        //Declaring variables
+        Double userCel;
+        double fahrenheit;
+
+        private void ConvertToFar(double fahrenheit)
+        {
+            //Calculates temperature in fahrenheit
+            fahrenheit = (userCel * 9 / 5) + 32;
+
+            //Display the fahrenheit
+            lblAnswer.Text = "The temperature in Fahrenheit is: " + fahrenheit + "°";
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //Get user input
+            userCel = Convert.ToDouble(txtCelsius.Text);
+
+            //Calling calculation function
+            this.ConvertToFar(fahrenheit);
+
+        }
+    }
+}
