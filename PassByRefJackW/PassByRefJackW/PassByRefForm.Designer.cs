20656c303c9bc03d8aaf12d45582c0cc39f8c40c
JackWrightMac <32547204+JackWrightMac@users.noreply.github.com>
2019-04-09 13:10:28 -0400

    Pass by reference added

---
+++ PassByRefJackW/PassByRefJackW/PassByRefForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace PassByRefJackW
+{
+    partial class frmPassByRef
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmPassByRef
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 364);
+            this.Name = "frmPassByRef";
+            this.Text = "Pass By Reference Jack W";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
