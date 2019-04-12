805ebbef5650f6d6c193a3718e18154a80edb7fd
JackWrightMac <32547204+JackWrightMac@users.noreply.github.com>
2019-04-12 13:06:45 -0400

    Address program added

---
+++ AddressProgramJackW/AddressProgramJackW/Address Program Form.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace AddressProgramJackW
+{
+    partial class frmAddressProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtApt = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtProvince = new System.Windows.Forms.TextBox();
+            this.txtPostalCode = new System.Windows.Forms.TextBox();
+            this.lblStatements = new System.Windows.Forms.Label();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtApt
+            // 
+            this.txtApt.Location = new System.Drawing.Point(406, 76);
+            this.txtApt.Name = "txtApt";
+            this.txtApt.Size = new System.Drawing.Size(100, 20);
+            this.txtApt.TabIndex = 2;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(406, 123);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtAddress.TabIndex = 3;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(406, 171);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 20);
+            this.txtCity.TabIndex = 4;
+            // 
+            // txtProvince
+            // 
+            this.txtProvince.Location = new System.Drawing.Point(406, 218);
+            this.txtProvince.Name = "txtProvince";
+            this.txtProvince.Size = new System.Drawing.Size(100, 20);
+            this.txtProvince.TabIndex = 5;
+            // 
+            // txtPostalCode
+            // 
+            this.txtPostalCode.Location = new System.Drawing.Point(406, 263);
+            this.txtPostalCode.Name = "txtPostalCode";
+            this.txtPostalCode.Size = new System.Drawing.Size(100, 20);
+            this.txtPostalCode.TabIndex = 6;
+            // 
+            // lblStatements
+            // 
+            this.lblStatements.AutoSize = true;
+            this.lblStatements.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lblStatements.Location = new System.Drawing.Point(95, 76);
+            this.lblStatements.Name = "lblStatements";
+            this.lblStatements.Size = new System.Drawing.Size(200, 216);
+            this.lblStatements.TabIndex = 7;
+            this.lblStatements.Text = "Apt. Number (optional)\r\n\r\nStreet Address\r\n\r\nCity\r\n\r\nProvince\r\n\r\nPostal Code\r\n";
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(272, 301);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(90, 23);
+            this.btnEnter.TabIndex = 8;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // frmAddressProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 336);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.lblStatements);
+            this.Controls.Add(this.txtPostalCode);
+            this.Controls.Add(this.txtProvince);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtApt);
+            this.Name = "frmAddressProgram";
+            this.Text = "Address Program by Jack W";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtApt;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtProvince;
+        private System.Windows.Forms.TextBox txtPostalCode;
+        private System.Windows.Forms.Label lblStatements;
+        private System.Windows.Forms.Button btnEnter;
+    }
+}
+
---
+++ AddressProgramJackW/AddressProgramJackW/Address Program Form.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddressProgramJackW
+{
+    public partial class frmAddressProgram : Form
+    {
+        public frmAddressProgram()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            string aptNumber = this.txtApt.Text;
+            string address = this.txtAddress.Text;
+            string city = this.txtCity.Text;
+            string province = this.txtProvince.Text;
+            string postalCode = this.txtPostalCode.Text;
+
+        }
+    }
+}
